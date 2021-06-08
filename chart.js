467ffb0c2524f5a5f305a52d087bf7560829f951
MARCELOPICCOLO <marcelociabatari@hotmail.com>
2021-06-08 17:02:47 -0300

    projeto concluido

--- chart.js
+++ chart.js
@@ -1,15 +1,10 @@
 $(document).ready(function(){
-    console.log('aaaaaaaaaaaa');
+    loadGrafico();
+    loadgrafico2();
+    loadtextos();
 })
 
-
-var ctx = document.getElementById('cv-veic').getContext('2d');
-
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul','Ago','Set','Out','Nov','Dez'],
-        datasets: [{
+let dados = [{
             label: 'Veículos monitorados em 2021',
             data: [300, 450, 700, 1200, 1250, 1500],
             backgroundColor: [
@@ -19,121 +14,108 @@ var myChart = new Chart(ctx, {
                 'rgba(255, 99, 132, 1)',            
             ],
             borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
+        }];
+
+let ctx1;
+let ctx2;
+
+let dados2 =[700,500,800,1200,950];
+
+function loadtextos(){
+    txt = '<div>'
+            +'<h5>veículos monitorados: '+dados2[4] +'</h5>'
+            +'<h5>veículos de carga: '+parseInt((60*dados2[4])/100)+'</h5>'
+            +'<h5>veículos pessoais: '+parseInt((25*dados2[4])/100)+'</h5>'
+            +'<h5>veículos carros fortes: '+parseInt((15*dados2[4])/100)+'</h5>'
+          +'</div>';
+
+    $("#textos").html(txt); 
+}
+
+function reset(){
+    var elemento = document.querySelector("#cv-veic");
+    var elemento2 = document.querySelector("#cv-veic-years");
+    elemento.parentNode.removeChild(elemento);
+    elemento2.parentNode.removeChild(elemento2);
+
+    var txt = '<canvas id="cv-veic"></canvas>';
+    var txt2 = '<canvas id="cv-veic-years"></canvas>';
+    $("#grafP").html(txt); 
+    $("#grafP2").html(txt2); 
+}
+
+function loadGrafico(){
+   
+    ctx1 = document.getElementById('cv-veic').getContext('2d');
+    
+        var myChart = new Chart(ctx1, {
+            type: 'line',
+            data: {
+                labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul','Ago','Set','Out','Nov','Dez'],
+                datasets: dados,
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+
+}
+
+
+$("#calc").click(function(){
+    dados[0].data[5] += parseInt($("#valor").val(), 10);
+    dados2[4]+=parseInt($("#valor").val(), 10);
+    reset();
+    loadGrafico();
+    loadgrafico2();
+    loadtextos();
+})
+
+
+function loadgrafico2(){
+
+    ctx2 = document.getElementById('cv-veic-years').getContext('2d');
+
+    var myChart = new Chart(ctx2, {
+        type: 'bar',
+        data: {
+            labels: ['2017','2018','20190','2020','2021'],
+            datasets: [{
+                label: 'Veículos monitorados anualmente',
+                data: dados2,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',              
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',          
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
             }
         }
-    }
-});
+    });
+
+}
 
 
-var ctx = document.getElementById('cv-veic-years').getContext('2d');
-
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['2017','2018','20190','2020','2021'],
-        datasets: [{
-            label: 'Veículos monitorados anualmente',
-            data: [700,500,800,1200,950],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',              
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',          
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
 
 
-var ctx = document.getElementById('cv-gast').getContext('2d');
 
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul','Ago','Set','Out','Nov','Dez'],
-        datasets: [{
-            label: 'Gastos em 2021',
-            data: [400000.00,390000.00,350000.00,310000.00,300000.00],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',              
-            ],
-            borderColor: [
-                'rgba(255, 159, 64, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',          
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
-
-
-var ctx = document.getElementById('cv-gast-years').getContext('2d');
-
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['2017','2018','20190','2020','2021'],
-        datasets: [{
-            label: 'Veículos monitorados anualmente',
-            data: [3000000.00,3400000.00,2900000.00,2500000.00,300000.00],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',              
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',          
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
--- index.php
+++ index.php
@@ -6,57 +6,67 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.2.1/chart.min.js">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?v=3">
     
     <title>DashBoard</title>
 </head>
 <body>
-    <div class="d-flex flex-column wrapper">
-        <header class="bg-dark">
-           
-                <nav class="navbar navbar-dark bg-dark justify-content-center">
-                    <span class="navbar-brand mb-0 h1">Securtty E&I</span>
-                </nav> 
-          
-        </header>
-        <main class="row d-flex flex-row wrapper bg-light">
-         
-                <div class="row">
-                    <div class="col-12 col-lg-4 bg-dark" style="background-color:#ccc">
-                        aaaaaa
-                    </div>
-                    <div class="col-12 col-lg-8" >
-                        <div class="row">
-                            <div class="col mt-3">
-                                <canvas id="cv-veic"></canvas>
-                            </div>
-                            <div class="col mt-3">
-                                <canvas id="cv-veic-years"></canvas>
-                            </div>
-                        </div>
+  <header class="text-center text-white">
+      <h3>Security E&I</h3>
+  </header>
+  <main class="section">
+      <div class="lateral">
+            <div class="form-group text-white">
+            <label for="exampleInputPassword1">Entrada do mês</label>
+            <input type="text" class="form-control" id="valor">
+            <button type="button" class="btn btn-primary mt-3" id="calc">Lançar Contrato</button>
+        </div>
+      </div>
+      <div class="painel">
+            <div class="grafico" id="grafP">
+                <canvas id="cv-veic"></canvas>                               
+            </div>
 
-                        <div class="row mt-5">
-                            <div class="col mt-3">
-                                <canvas id="cv-gast"></canvas>
-                            </div>
-                            <div class="col mt-3">
-                                <canvas id="cv-gast-years"></canvas>
-                            </div>
-                        </div>
+            <div class="grafico" id="grafP2">
+                <canvas id="cv-veic-years"></canvas>
+            </div>
 
-                    </div>
-                </div>
-                  
-        </main>
-        <footer>
-     
-        </footer>
-    </div>
+            <div class="textos" id="textos">
+                
+            </div>
+
+        </div>
+  </main>
     
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<script src="chart.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="chart.js?v=2"></script>
 </body>
 </html>
+
+<!--
+     <div class="col-2 bg-dark text-white p-3">
+           
+           </div>
+           <div class="col">
+               <div class="row d-flex wrap">
+                    <div class="col mt-3">
+                            <canvas id="cv-veic"></canvas>
+                    </div>                      
+                    <div class="col mt-3">
+                            <canvas id="cv-veic-years"></canvas>
+                    </div>
+                </div>
+                <div class="row mt-4">
+                    <div class="col mt-3" >
+                            <canvas id="cv-services"></canvas>
+                    </div>
+
+                    <div class="col mt-4">
+                             
+                    </div>
+                </div>
+
+           </div>
